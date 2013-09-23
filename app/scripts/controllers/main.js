ffa78cc310fa6196cbfbcf8b7de822431ece71ea
Rogerz Zhang <rogerz.zhang@gmail.com>
2013-09-23 20:13:26 +0800

    Generated by yo

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('swallApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
