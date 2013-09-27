d00738e22bda34562ea48086a9344cd9bf4db831
Rogerz Zhang <rogerz.zhang@gmail.com>
2013-09-27 20:37:57 +0800

    Build self containted dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -249,7 +249,7 @@ module.exports = function (grunt) {
           src: [
             '*.{ico,png,txt}',
             '.htaccess',
-            'bower_components/**/*',
+//            'bower_components/**/*',
             'images/{,*/}*.{gif,webp}',
             'styles/fonts/*'
           ]
