9f1919aedfa7bc65fd9a70ef0ffa52963d98f964
Rogerz Zhang <rogerz.zhang@gmail.com>
2013-09-27 23:16:24 +0800

    Fix travis error

---
+++ .travis/install_chrome.sh
@@ -0,0 +1,3 @@
+sudo sh -c "echo 'deb http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google-chrome.list"
+sudo apt-get update --force-yes
+sudo apt-get install --force-yes google-chrome-stable
