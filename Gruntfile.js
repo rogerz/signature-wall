b3aac56ade84e891478ef1630a54737d627a0a2b
Rogerz Zhang <rogerz.zhang@gmail.com>
2013-09-27 21:20:14 +0800

    Fix build issue

--- Gruntfile.js
+++ Gruntfile.js
@@ -260,6 +260,13 @@ module.exports = function (grunt) {
           src: [
             'generated/*'
           ]
+        }, {
+          expand: true,
+          cwd: '<%= yeoman.app %>/bower_components/bootstrap',
+          dest: '<%= yeoman.dist %>',
+          src: [
+            'fonts/*'
+          ]
         }]
       },
       styles: {
