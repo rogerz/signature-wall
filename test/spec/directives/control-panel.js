50cef9e6c74de385c072e79dabf89861bbc07488
Rogerz Zhang <rogerz.zhang@gmail.com>
2013-10-06 16:02:35 +0800

    Add test case for control panel

--- test/spec/directives/control-panel.js
+++ test/spec/directives/control-panel.js
@@ -15,8 +15,17 @@ describe('Directive: controlPanel', function () {
     scope.$digest();
   }));
 
-  it('should have a toggle button', inject(function () {
-    var toggle = element.find('#sb-hot-zone li a');
+  it('should toggle inactive on click', inject(function ($timeout) {
+    expect(scope.inactive).toBe(true);
+    var toggle = element.find('#sb-toggle')
+      , sideBar = element.find('#side-bar');
     expect(toggle.length).toBe(1);
+    expect(sideBar.length).toBe(1);
+    toggle.click();
+    $timeout.flush();
+    expect(sideBar.hasClass('inactive')).toBe(false);
+    toggle.click();
+    $timeout.flush();
+    expect(sideBar.hasClass('inactive')).toBe(true);
   }));
 });
